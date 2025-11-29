e3b4feefb488e8e24d1438d20a0defb2a4e021dc
Konstantin Komelin <konstantin.komelin@gmail.com>
2025-11-29 13:33:38 +0100

    Set the network for MerSuiProvider explicitly.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MerSuiProvider>
+    <MerSuiProvider network="mainnet">
       <App />
     </MerSuiProvider>
   </StrictMode>
